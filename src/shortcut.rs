23db784855a3b4bb4eba1bfe59e561fadbe030b7
Tom Groenwoldt <tom.groenwoldt@gmail.com>
2023-03-28 00:35:08 +0200

    Add solution button and improve styling

--- src/shortcut.rs
+++ src/shortcut.rs
@@ -3,7 +3,13 @@ use yew::{classes, html, Component, Html, Properties};
 /// Component which renders the input fields for the user.
 /// The number of input fields directly depend on the length
 /// of the solution length.
-pub struct Shortcut;
+pub struct Shortcut {
+    view_solution: bool,
+}
+
+pub enum Msg {
+    ShowSolution,
+}
 
 #[derive(Properties, Clone, PartialEq)]
 pub struct ShortcutProperties {
@@ -14,25 +20,50 @@ pub struct ShortcutProperties {
 }
 
 impl Component for Shortcut {
-    type Message = ();
+    type Message = Msg;
 
     type Properties = ShortcutProperties;
 
     fn create(_ctx: &yew::Context<Self>) -> Self {
-        Self
+        Self {
+            view_solution: false,
+        }
+    }
+
+    fn update(&mut self, _ctx: &yew::Context<Self>, msg: Self::Message) -> bool {
+        match msg {
+            Msg::ShowSolution => self.view_solution = !self.view_solution,
+        }
+        true
+    }
+
+    fn changed(&mut self, _ctx: &yew::Context<Self>, _old_props: &Self::Properties) -> bool {
+        // If the solution button was activated, and a user starts to type again, we disable the button.
+        // On a skip we do this as well.
+        self.view_solution = false;
+        true
     }
 
     fn view(&self, ctx: &yew::Context<Self>) -> Html {
+        let onclick = ctx.link().callback(|_| Msg::ShowSolution);
+        let class = if self.view_solution { "active" } else { "" };
         html! {
             <div class="shortcut">
-                // Render an input field for every solution element.
-                {for ctx.props()
-                    .solution
-                    .iter()
-                    .enumerate()
-                    .map(|(index, key)|
-                        // Set `render_plus` for every key, except the first.
-                        Self::render_key(key, ctx.props().guess.get(index), index != 0))
+                <div class="input">
+                    // Render an input field for every solution element.
+                    {for ctx.props()
+                        .solution
+                        .iter()
+                        .enumerate()
+                        .map(|(index, key)|
+                            // Set `render_plus` for every key, except the first.
+                            Self::render_key(key, ctx.props().guess.get(index), index != 0, self.view_solution))
+                    }
+                </div>
+                if !ctx.props().solution.is_empty() {
+                    <div class="solution">
+                        <div {class} {onclick}>{"Show solution"}</div>
+                    </div>
                 }
             </div>
         }
@@ -41,7 +72,12 @@ impl Component for Shortcut {
 
 impl Shortcut {
     /// Renders the key. Distinguishes between correct, wrong and empty user input.
-    fn render_key(key: &str, guess: Option<&String>, render_plus: bool) -> Html {
+    fn render_key(
+        key: &str,
+        guess: Option<&String>,
+        render_plus: bool,
+        view_solution: bool,
+    ) -> Html {
         let class = if let Some(c) = guess {
             let mut class = String::from("");
             if c.eq(key) {
@@ -63,9 +99,11 @@ impl Shortcut {
                 if render_plus {
                     <span class="plus">{"+"}</span>
                 }
-                <div class={classes!("key", class)}>
-                    {guess}
-                </div>
+                if view_solution {
+                    <div class="key solution-key">{key}</div>
+                } else {
+                    <div class={classes!("key", class)}>{guess}</div>
+                }
             </>
         }
     }
